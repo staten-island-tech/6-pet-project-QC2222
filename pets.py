64295c7ccba652f04cde93fde6c336f331008980
QC2222 <quintonc12@nycstudents.net>
2026-03-23 10:17:21 -0400

    1
    1

---
+++ pets.py
@@ -0,0 +1,8 @@
+class pet:
+    def __init__(self, name, inventory):
+        self.name = name
+        self.inventory = inventory
+availablepets = ["dog", "cat", "turtle", "fish", "bird", "komodo dragon"]
+while x != 1:
+    for i in availablepets:
+        petselec = input("Choose your pet: Dog, cat, turtle, fish, bird or komodo dragon.")
