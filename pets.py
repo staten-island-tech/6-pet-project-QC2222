ed4b225f7b7d272c6901a7692a7201f6906fd884
QC2222 <quintonc12@nycstudents.net>
2026-03-27 10:17:16 -0400

    1
    1

--- pets.py
+++ pets.py
@@ -1,18 +1,17 @@
+import time
 class pet:
-    def __init__(self, name, specimen, happiness, entertainment, hunger):
+    def __init__(self, name, happiness, entertainment, hunger):
         self.name = name
-        self.specimen = specimen
-        self.inventory = inventory
         self.happiness = happiness
         self.entertainment = entertainment
         self.hunger = hunger
-    def showstats(self, name, specimen, happiness, entertainment, hunger):
+    def showstats(self, name, happiness, entertainment, hunger):
         print("Name", name)
-        print("Specimen", specimen)
         print("Happiness", happiness)
         print("Entertainment", entertainment)
         print("Hunger", hunger)
 availablepets = ["dog", "cat", "turtle", "fish", "bird", "komodo dragon"]
-while x != 1:
-    for i in availablepets:
-        petselec = input("Choose your pet: Dog, cat, turtle, fish, bird or komodo dragon.")
+Charlie = pet("Charlie", 100, 100, 100)
+while 0 != 1:
+    time.sleep(5)
+    Charlie.showstats("Charlie", 100, 100, 100)
