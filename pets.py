16efab7e51bf77ff19eb00b36c3c56767e389ec6
QC2222 <quintonc12@nycstudents.net>
2026-04-15 10:16:55 -0400

    12
    1

--- pets.py
+++ pets.py
@@ -1,17 +1,30 @@
 import time
+import random
 class pet:
     def __init__(self, name, happiness, entertainment, hunger):
         self.name = name
         self.happiness = happiness
         self.entertainment = entertainment
         self.hunger = hunger
-    def showstats(self, name, happiness, entertainment, hunger):
-        print("Name", name)
-        print("Happiness", happiness)
-        print("Entertainment", entertainment)
-        print("Hunger", hunger)
-availablepets = ["dog", "cat", "turtle", "fish", "bird", "komodo dragon"]
+    def terminal(self):
+        print("Pet name: ", self.name)
+        print("Commands:")
+        print("\"stats\"")
+        print("\"feed\"")
+        print("\"play\"")
 Charlie = pet("Charlie", 100, 100, 100)
-while 0 != 1:
-    time.sleep(5)
-    Charlie.showstats("Charlie", 100, 100, 100)
+while Charlie.hunger > 0 or Charlie.happiness > 0:
+    Charlie.terminal()
+    command = input().lower()
+    if command == "stats":
+        print("Name", Charlie.name)
+        print("Happiness", Charlie.happiness)
+        print("Entertainment", Charlie.entertainment)
+        print("Hunger", Charlie.hunger)
+    elif command == "feed":
+        if Charlie.hunger > 90:
+            print("He's full.")
+        else:
+            Charlie.hunger += randint(5, 15)
+    elif command == "play":
+        Charlie.happiness += (20/(Charlie.happiness))//1
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+sys.set_int_max_str_digits(100000000)
+x = 1
+while 0 != 1:
+    x *= 3
+    print(x)
