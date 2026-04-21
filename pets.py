a5a11f3423afc0558b90af3daf9942ee3c45cc07
QC2222 <quintonc12@nycstudents.net>
2026-04-21 10:16:56 -0400

    bigupdate

--- pets.py
+++ pets.py
@@ -1,62 +1,84 @@
 import time
 import random
 class pet:
-    def __init__(self, name, hapyns, hngr, thrs):
+    def __init__(self, name, hapyns, hngr, thrs, hygn):
         self.name = name
         self.hapyns = hapyns
         self.hngr = hngr
         self.thrs = thrs
+        self.hygn = hygn
     def terminal(self):
         print("")
         print("Pet name: ", self.name)
-        print("Happiness:", (round(self.hapyns), 1))
+        print("Happiness:", round((self.hapyns), 1))
         print("Hunger:", self.hngr)
         print("Thirst:", self.thrs)
+        print("Hygiene:", self.hygn)
         print("Commands:")
         print("\"feed\"")
         print("\"play\"")
         print("\"give water\"")
-        print("\"do nothing\"")
+        print("\"wash\"")
+        print("\"nothing\"")
+        print("\"rename\"")
         print("")
-Charlie = pet("Charlie", 100, 100, 100)
-print(Charlie.hapyns)
-while Charlie.hngr >= 0 and Charlie.thrs >= 0 and Charlie.hapyns >= 0:
-    Charlie.terminal()
+    def feedpet(self):
+        if self.hngr >= 98:
+            print(self.name, " is full.")
+            validcmd = 0
+        else:
+            self.hngr += random.randint(15, 35)
+            print(self.name, " fed.")
+    def play(self):
+        self.hapyns += round((20 - (5 * (1 + (1 - (self.hngr / 100))))), 1)
+        self.hngr -= random.randint(2, 7)
+        self.thrs -= random.randint(2, 7)
+        print("Playing with ", self.name, "...")
+    def wateringthedog(self):
+        if self.thrs > 98:
+            print(self.name, " is not thirsty.")
+            validcmd = 0
+        else:
+            self.thrs += random.randint(15, 35)
+            print(self.name, " drank.")
+    def wash(self):
+        self.hygn += randint(10, 15)
+        print("Washing ", self.name)
+    def namepet(self):
+        self.name = input("Name your pet: ")
+thepet = pet("name", 100, 100, 100, 100)
+while thepet.hngr >= 0 and thepet.thrs >= 0 and thepet.hapyns >= 0:
+    thepet.namepet()
+    thepet.terminal()
     command = input().lower()
-    if command == "feed":
-        if Charlie.hngr > 95:
-            print("He's full.")
+    print("")
+    validcmd = 0
+    while validcmd == 0:
+        validcmd = 1
+        thepet.hygn -= 2
+        if command == "feed":
+            thepet.feedpet()
+        elif command == "water" or command == "give water":
+            thepet.wateringthedog()
+        elif command == "wash":
+            thepet.wash()
+        elif command == "do nothing" or command == "nothing":
+            break
+        elif command == "rename":
+            thepet.rename()
+            print("You bought a new nametag.")
         else:
-            Charlie.hngr += random.randint(20, 35)
-            print("Charlie fed.")
-        time.sleep(2)
-    elif command == "play":
-        Charlie.hapyns += round((20 - (5 * (1 + (1 - (Charlie.hngr / 100))))), 1)
-        Charlie.hngr -= random.randint(2, 7)
-        Charlie.thrs -= random.randint(2, 7)
-        print("Playing with Charlie...")
-        time.sleep(2)
-    elif command == "give water":
-        if Charlie.thrs > 95:
-            print("He's not thirsty.")
-        else:
-            Charlie.thrs += random.randint(20, 30)
-            print("Charlie drank.")
-        time.sleep(2)
-    elif command == "do nothing":
-        print("Doing nothing...")
-        time.sleep(2)
-    else:
-        print("Invalid command, input something else.")
-        time.sleep(2)
-    Charlie.hngr -= random.randint(3, 5)
-    Charlie.thrs -= random.randint(3, 5)
-    Charlie.hapyns -= round((random.randint(2, 3) * ((1 + (1 - (Charlie.hngr / 100)))**2)), 1)
-    if Charlie.hapyns > 100:
-        Charlie.hapyns = 100
-if Charlie.hngr <= 0:
-    print("Charlie died of starvation, this is all your fault, you didn't feed him, what is wrong with you.")
-elif Charlie.thrs <= 0:
-    print("Charlie died of thirst, why would you not take care of him, how could you do this to him, he's literally dead.")
+            print("Invalid command")
+            validcmd = 0
+            thepet.hygn += 2
+    thepet.hngr -= random.randint(3, 5)
+    thepet.thrs -= random.randint(3, 5)
+    thepet.hapyns -= round((random.randint(2, 3) * ((1 + (1 - (thepet.hngr / 100)))**2)), 1)
+    thepet.hygn -= 1
+    time.sleep(2)
+if thepet.hngr <= 0:
+    print(thepet.name, " starved to death.")
+elif thepet.thrs <= 0:
+    print(thepet.name, " died of thirst. Pretty unfortunate actually.")
 else:
-    print("Charlie ran away because you were a terrible owner, you couldn't even keep him happy")
+    print(thepet.name, " ran away because you were a terrible owner. ")
